d1d2bb13c96a5fbb6f91a5dbe4fa200e21f2689d
YoungRok <51745277+robin9804@users.noreply.github.com>
2020-08-11 15:59:36 +0900

    bug fix

--- Generation/ASM_FFT.py
+++ Generation/ASM_FFT.py
@@ -8,11 +8,13 @@ mm = 1e-3
 um = 1e-6
 nm = 1e-9
 
-
 class Propagation:
     """
     ASM Propagation simulation using multi-core processing
     """
+    wvl_R = 638 * nm
+    wvl_G = 520 * nm
+    wvl_B = 450 * nm
     def __init__(self, z, plypath, angleX=0, angleY=0, pixel_pitch=3.6 * um, scaleXY=0.03, scaleZ=0.25, w=3840, h=2160):
         self.z = z
         self.pp = pixel_pitch
@@ -20,20 +22,18 @@ class Propagation:
         self.scaleZ = scaleZ
         self.width = w
         self.height = h
-        self.angleX = angleX
-        self.angleY = angleY
-        self.thetaX = self.angleX * (np.pi / 180)
-        self.thetaY = self.angleY * (np.pi / 180)
+        self.thetaX = angleX * (np.pi / 180)
+        self.thetaY = angleY * (np.pi / 180)
         with open(plypath, 'rb') as f:
             self.plydata = plyfile.PlyData.read(f)
             self.plydata = np.array(self.plydata.elements[1].data)
-        self.wvl_R = 638 * nm
-        self.wvl_G = 520 * nm
-        self.wvl_B = 450 * nm
         self.num_cpu = multiprocessing.cpu_count()
         self.num_point = [i for i in range(len(self.plydata))]
         self.sizeX = self.pp * self.width
         self.sizeY = self.pp * self.height
+        self.ps = self.scaleXY / self.width  # source plane pixel pitch (sampling rate)
+        self.Wr = self.pp * self.width  # Receiver plane width
+        self.Ws = self.scaleXY  # Source plane width
 
     def k(self, wvl):
         return (np.pi *2) / wvl
@@ -44,44 +44,47 @@ class Propagation:
     def ifft(self, f):
         return np.fft.fftshift(np.fft.ifft2(np.fft.fftshift(f)))
 
+    def Nzp(self, zz, wvl):
+        """Number of zero padding"""
+        p = (wvl * zz) / np.sqrt(4 * self.ps * self.ps - wvl * wvl)
+        return np.int((1 / self.ps) * (self.Wr/2 -self.Ws/2 + p) + 1)
+
+    def point_map(self, x1, y1, zz, wvl):
+        """find object point map"""
+        N = self.Nzp(zz, wvl)
+        W = N + self.width  # zero padding
+        b = np.zeros((W, W))
+        p = np.int((x1 + 1) * (self.width / 2))
+        q = np.int((1 - y1) * (self.width / 2))
+        b[q + N//2 , p + N//2] = 1
+        print(p,' and ', q)
+        return b
+
     def asm_kernel(self, zz, wvl):
         """
         ASM kernel
         """
-        a = np.zeros((self.height, self.width), dtype='complex128')
-        for i in range(self.height):
-            for j in range(self.width):
-                # fx = (j - self.width/2) / (self.width * self.pp)
-                # fy = -(i - self.height/2) / (self.height * self.pp)
-                fx = (-1 / (2 * self.pp) + j * (1/self.sizeX))
-                fy = (1 / (2 * self.pp) - (i+1) * (1/self.sizeY))
-                if np.sqrt(fx ** 2 + fy ** 2) < 1 / wvl:
-                    fx = fx * wvl
-                    fy = fy * wvl
-                    a[i, j] = np.exp(1j * self.k(wvl) * zz * np.sqrt(1 - fx ** 2 - fy ** 2))
+        N = self.Nzp(zz, wvl)
+        W = N + self.width
+        deltaK = (2 * np.pi) / (W * self.ps)
+        a = np.zeros((W, W), dtype='complex128')
+        for i in range(W):
+            for j in range(W):
+                fx = ((i - W/2) * deltaK) / self.k(wvl)
+                fy = ((j - W/2) * deltaK) / self.k(wvl)
+                if np.sqrt(fx * fx + fy * fy) < (1 / wvl)**3:
+                    a[i, j] = np.exp(1j * self.k(wvl) * zz * np.sqrt(1 - fx * fx - fy * fy))
         print(zz, 'kernel ready')
         return a
 
-    def Refwave(self, wvl):
+    def Refwave(self, wvl, r):
         a = np.zeros((self.height, self.width), dtype='complex128')
         for i in range(self.height):
             for j in range(self.width):
                 x = (j - self.width/2) * self.pp
                 y = -(i - self.height/2) * self.pp
-                a[i, j] = np.exp(self.k(wvl) * (x * np.sin(self.thetaX) + y * np.sin(self.thetaY)))
-        return a
-
-    def point_map(self, x1, y1):
-        """find object point map"""
-        a = np.zeros((self.height, self.height))  # 별도의 scale form이 필요하다.
-        b = np.zeros((self.height, self.width))
-        res = self.height / 2
-        p = np.int(((x1 + self.scaleXY)/self.scaleXY) * res)
-        q = np.int(((self.scaleXY - y1)/self.scaleXY) * res)
-        a[q, p] = 1
-        print(p,' and ', q)
-        b[:, (self.width - self.height) // 2 : (self.width + self.height) // 2] = a
-        return b
+                a[i, j] = np.exp(-1j * self.k(wvl) * (x * np.sin(self.thetaX) + y * np.sin(self.thetaY)))
+        return a / r
 
     def ASM_FFT(self, n, wvl):
         """
@@ -93,14 +96,21 @@ class Propagation:
             color = 'blue'
         else:
             color = 'red'
-        x0 = self.plydata['x'][n] * self.scaleXY
-        y0 = self.plydata['y'][n] * self.scaleXY
-        pmap = self.point_map(x0, y0)
+        x0 = self.plydata['x'][n]
+        y0 = self.plydata['y'][n]
         zz = self.z - self.plydata['z'][n] * self.scaleZ
+        # point map
+        W = self.Nzp(zz, wvl) + self.width
+        pmap = np.zeros((W, W))
+        p = np.int((x0 + 1) * (self.width / 2))
+        q = np.int((1 - y0) * (self.width / 2))
+        pmap[q + self.Nzp(zz, wvl)//2 , p + self.Nzp(zz, wvl)//2] = 1
+        print(n, 'th p map done')
         amp = self.plydata[color][n] * pmap
         amp = self.fft(amp)
         ch = self.ifft(amp * self.asm_kernel(zz, wvl))
-        ch = ch * self.Refwave(wvl)
+        ch = ch[(W-self.height)//2 : (W+self.height)//2, (W-self.width)//2 : (W+ self.width)//2]
+        ch = ch * self.Refwave(wvl, zz)
         print(n, ' point', color ,' is done')
         return ch
 
@@ -157,7 +167,7 @@ class Propagation:
         img[:, :, 0] = self.normalize(R, type)
         img[:, :, 1] = self.normalize(G, type)
         img[:, :, 2] = self.normalize(B, type)
-        img = img[840:3000, :, :]  # crop
+        #img = img[840:3000, :, :]  # crop
         plt.imsave(fname, img)
         return img
 
@@ -176,7 +186,13 @@ class Propagation:
 
 
 if __name__ == '__main__':
-    p = Propagation(0.5,'point_3.ply', h=3840)
+    p = Propagation(0.5,'point_3.ply', angleY=12)
     print(p.plydata.shape)
-    p.colorimg('ASM_3point_offaxis_7.bmp')
-    #p.singlechannel('ASM_Green_2.bmp', p.wvl_G)
+    #p.colorimg('ASM_3point_offaxis_10.bmp')
+    p.singlechannel('ASM_Green_3.bmp', p.wvl_G)
+    print(p.pp)
+    print(p.k(p.wvl_R))
+    #ref = p.Refwave(p.wvl_R, 1)
+    #print(ref)
+    #plt.imshow(np.real(ref))
+    #plt.show()
