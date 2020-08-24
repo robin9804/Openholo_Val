aecdb316a8682ef56fcb041256786cb87380efc2
YoungRok <51745277+robin9804@users.noreply.github.com>
2020-08-24 11:41:30 +0900

    Update Fresnel_integral.py
    ignore anti-aliasing

--- PointCloud/functions/Fresnel_integral.py
+++ PointCloud/functions/Fresnel_integral.py
@@ -26,22 +26,33 @@ scaleZ = 0.25
 def k(wvl):
     return (np.pi * 2) / wvl
 
+
 @njit(nogil=True)
-def h_Fresnel(x1, y1, x2, y2, z, wvl, thetaX, thetaY):
+def h_Fresnel(x1, y1, x2, y2, z, wvl):
     """impulse response function of Fresnel propagation method"""
     r = ((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2)) / (2*z)
-    t = (wvl * z) / (2 * pp)
-    if (x1 - t < x2 < x1 + t) and (y1 - t < y2 < y1 + t):  # anti aliasing
-        h_r = np.cos(k(wvl) * (r + x2 * np.sin(thetaX) + y2 * np.sin(thetaY)))
-        h_i = np.sin(k(wvl) * (r + x2 * np.sin(thetaX) + y2 * np.sin(thetaY)))
-    else:
-        h_r = 0
-        h_i = 0
+    # t = (wvl * z) / (2 * pp) 안티엘리어싱 무시
+    h_r = np.cos(k(wvl) * r)
+    h_i = np.sin(k(wvl) * r)
     return h_r, h_i
 
 
+@njit(nogil=True, cache=True)
+def Refwave(wvl, r, thetaX, thetaY):
+    """Reference wave"""
+    a = np.zeros((h, w))
+    b = np.zeros((h, w))
+    for i in range(h):
+        for j in range(w):
+            x = (j - w/2) * pp
+            y = -(i - h/2) * pp
+            a[i, j] = np.cos(k(wvl) * (x * np.sin(thetaX) + y * np.sin(thetaY)))
+            b[i, j] = np.sin(k(wvl) * (x * np.sin(thetaX) + y * np.sin(thetaY)))
+    return a / r,  b / r
+
+
 @njit(nogil=True)
-def Conv(x1, y1, z1, z2, amp, wvl, thetaX, thetaY):
+def Conv(x1, y1, z1, z2, amp, wvl):
     zz = z2 - z1
     ch_r = np.zeros((h, w))
     ch_i = np.zeros((h, w))
@@ -49,7 +60,7 @@ def Conv(x1, y1, z1, z2, amp, wvl, thetaX, thetaY):
         for j in range(w):
             x2 = (j - w / 2) * pp
             y2 = -(i - h / 2) * pp
-            re, im = h_Fresnel(x1, y1, x2, y2, zz, wvl, thetaX, thetaY)
+            re, im = h_Fresnel(x1, y1, x2, y2, zz, wvl)
             ch_r[i, j] = re
             ch_i[i, j] = im
     print('point done')
@@ -80,7 +91,7 @@ class Frsn(Encoding):
         y0 = self.plydata['y'][n] * scaleXY
         z0 = self.plydata['z'][n] * scaleZ
         amp = self.plydata[color][n] * (self.z / wvl)
-        ch = Conv(x0, y0, z0, self.z, amp, wvl, self.thetaX, self.thetaY)
+        ch = Conv(x0, y0, z0, self.z, amp, wvl)
         print(n, ' th point ', color, ' Done')
         return ch
 
