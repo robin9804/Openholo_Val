a8a3ed73d260db9684a5d0961df93e1e0d9d4589
YoungRok <51745277+robin9804@users.noreply.github.com>
2020-08-28 13:28:39 +0900

    Update 2D-RS.py
    RGB 보정

--- 2Dimg/2D-RS.py
+++ 2Dimg/2D-RS.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 from PIL import Image
 from depthmap.encoding import Encoding
 from concurrent.futures import ProcessPoolExecutor
-import multiprocessing
 from numba import njit
 
 # parameters
@@ -15,23 +14,30 @@ wvl_G = 525 * nm  # Green
 wvl_B = 463 * nm  # Blue
 
 # SLM parameters
-w = 1920  # width
-h = 1080  # height
-pp = 3.6 * um  # SLM pixel pitch
+w = 3840            # width
+h = 2160            # height
+pp = 3.6 * um       # SLM pixel pitch
 scaleXY = 0.03
 scaleZ = 0.25
 
-fname = 'original.bmp'
+# source parameters
+w_s = 1920              # source width
+h_s = 1080              # height
+ss = int(h_s * w_s)     # source size
+ps = scaleXY / w_s      # source plane pixel pitch
+
 
 @njit(nogil=True, cache=True)
 def k(wvl):
     return (np.pi * 2) / wvl
 
+
 @njit(nogil=True)
 def h_RS(x1, y1, z1, x2, y2, z2, wvl):
     """Impulse Response of R-S propagation"""
-    r = np.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2))
-    t = (wvl * r) / (2 * pp)  # anti alliasing conditio
+    zz = (z2 - z1) * (wvl / wvl_B)  # point cloud 거리 좌표 보정
+    r = np.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + zz * zz)
+    t = (wvl * r) / (2 * pp)  # anti aliasing condition
     if (x1 - t < x2 < x1 + t) and (y1 - t < y2 < y1 + t):
         h_r = np.sin(k(wvl) * r)
         h_i = np.cos(k(wvl) * r)
@@ -40,62 +46,62 @@ def h_RS(x1, y1, z1, x2, y2, z2, wvl):
         h_i = 0
     return h_r / (r * r), h_i / (r * r)
 
+
 @njit(nogil=True)
-def pointConv(x1, y1, z, wvl, amp):
-    u_re = np.zeros((h,w))
-    u_im = np.zeros((h,w))
+def point_conv(n, image, z, wvl):
+    ch_r = np.zeros((h, w))
+    ch_i = np.zeros((h, w))
+    x_s = int(n % w_s)
+    y_s = int(n // w_s)
+    amp = image[y_s, x_s]
+    x1 = (x_s - w_s / 2) * ps  # source plane 좌표
+    y1 = -(y_s - h_s / 2) * ps
     for i in range(h):
         for j in range(w):
             x2 = (j - w / 2) * pp
             y2 = -(i - h / 2) * pp
             re, im = h_RS(x1, y1, 0, x2, y2, z, wvl)
-            re = re * amp
-            im = im * amp
-            u_re[i, j] = re
-            u_im[i, j] = im
-    # print(x1, ', ', y1, 'done')
-    return u_re, u_im
+            ch_r[i, j] = re
+            ch_i[i, j] = im
+    ch_r = ch_r * amp
+    ch_i = ch_i * amp
+    # print(n, 'point done')
+    return ch_r + 1j * ch_i
 
 
 class RS(Encoding):
     def __init__(self, imgpath, f=1):
         self.z = f  # Propagation distance
         self.imagein = np.asarray(Image.open(imgpath))
-        self.num_cpu = multiprocessing.cpu_count()  # number of CPU
-        self.imgR = np.double(self.imagein[:, :, 0])
-        self.imgG = np.double(self.imagein[:, :, 1])
-        self.imgB = np.double(self.imagein[:, :, 2])
-        self.num_point = [i for i in range(w)]
-
-    def rowConv(self,image, row, wvl):
-        u_re = np.zeros((h, w))
-        u_im = np.zeros((h, w))
-        for n in range(h):
-            if image[n, row] == 0:
-                continue
-            amp = image[n, row]
-            x1 = (row - w / 2) * pp
-            y1 = (n - h / 2) * pp
-            re, im = pointConv(x1, y1, 0, amp, wvl)
-            u_re += re
-            u_im += im
-            print(n, ' raw ready')
-        return u_re + 1j * u_im
+        self.num_cpu = 16 #multiprocessing.cpu_count()  # number of CPU
+        self.img_R = np.double(self.imagein[:, :, 0])
+        self.img_G = np.double(self.imagein[:, :, 1])
+        self.img_B = np.double(self.imagein[:, :, 2])
+        self.num_point = []  # [i for i in range(ss)]     # number of point
+        for i in range(ss):
+            x_s = int(i % w_s)
+            y_s = int(i // w_s)
+            if self.img_R[y_s, x_s] == 0 and self.img_G[y_s, x_s] == 0 and self.img_B[y_s, x_s] == 0:
+                pass
+            else:
+                self.num_point.append(i)
 
     def Cal(self, row, color='red'):
         """Convolution"""
+        # ch = np.zeros((h, w), dtype='complex128')
         if color == 'green':
             wvl = wvl_G
-            img = self.imgG
+            image = self.img_G
         elif color == 'blue':
             wvl = wvl_B
-            img = self.imgB
+            image = self.img_B
         else:
             wvl = wvl_R
-            img = self.imgR
-        ch = self.rowConv(img, row, wvl)
-        print(row, ' th point ', color, ' Done')
+            image = self.img_R
+        ch = point_conv(row, image, self.z, wvl)
+        print(row % w_s, ',', row//w_s ,' th point ', color, ' Done')
         return ch
+
 
     def Conv_R(self, n):
         return self.Cal(n, 'red')
@@ -117,7 +123,7 @@ class RS(Encoding):
         print(self.num_cpu, " core Ready")
         ch = np.zeros((h, w), dtype='complex128')
         count = np.split(self.num_point, [i * self.num_cpu for i in range(1, w // self.num_cpu)])
-        print(count)
+        print(len(count))
         for n in count:
             with ProcessPoolExecutor(self.num_cpu) as ex:
                 cache = [result for result in ex.map(func, list(n))]
