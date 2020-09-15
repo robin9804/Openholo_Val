61db0a542bdcd316d74f33e36534300e8177fffc
YoungRok <51745277+robin9804@users.noreply.github.com>
2020-09-15 17:01:51 +0900

    Update FFT.py

--- 2Dimg/FFT.py
+++ 2Dimg/FFT.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
-from openholo.funcs.encoding import Encoding
 from numba import njit
 
 # parameters
@@ -30,6 +29,7 @@ def limits(u, z, wvl):
     # u is delta u
     return (1/wvl) * np.sqrt((2 * u * z)**2 + 1)
 
+
 @njit
 def asm_kernel(wvl, z):
     deltax = 1 / (w * pp * 4)     # sampling period
@@ -39,10 +39,10 @@ def asm_kernel(wvl, z):
     for i in range(w*2):
         for j in range(h*2):
             fx = ((i - w) * deltax)
-            fy = ((j - h) * deltay)
+            fy = -((j - h) * deltay)
             delx = limits(fx, z, wvl)
             dely = limits(fy, z, wvl)
-            if -delx < fx < delx and -dely < fy < dely: # band limiting
+            if -delx < fx < delx and -dely < fy < dely:
                 #(fx * fx + fy * fy) < (1 / (wvl * wvl)):
                 a[j, i] = np.cos(2 * np.pi * z * np.sqrt((1/wvl)**2 - fx * fx - fy * fy))
                 b[j, i] = np.sin(2 * np.pi * z * np.sqrt((1/wvl)**2 - fx * fx - fy * fy))
@@ -57,25 +57,24 @@ def h_frsn(pixel_pitch_x, pixel_pitch_y, nx, ny, zz, wvl):
     for i in range(nx):
         for j in range(ny):
             x = (i - nx / 2) * pixel_pitch_x
-            y = (j - ny / 2) * pixel_pitch_y
+            y = -(j - ny / 2) * pixel_pitch_y
             re[j, i] = np.cos((np.pi / (wvl * zz)) * (x * x + y * y))
             im[j, i] = np.sin((np.pi / (wvl * zz)) * (x * x + y * y))
-    return re - 1j * im
+    return re + 1j * im
 
 
-class FFT(Encoding):
+class FFT:
     def __init__(self, imgpath, f=1, angleX=0, angleY=0):
         self.zz = f
         self.imagein = np.asarray(Image.open(imgpath))
         self.thetaX = angleX * (np.pi / 180)
         self.thetaY = angleY * (np.pi / 180)
-        self.img_R = np.double(self.resizeimg(wvl_R, self.imagein[:, :, 0])) / 255
-        self.img_G = np.double(self.resizeimg(wvl_G, self.imagein[:, :, 1])) / 255
-        self.img_B = np.double(self.resizeimg(wvl_B, self.imagein[:, :, 2])) / 255
-        # asm 용
-        self.img_r = np.double(self.resizeimg(wvl_B, self.imagein[:, :, 0])) / 255
-        self.img_g = np.double(self.resizeimg(wvl_B, self.imagein[:, :, 1])) / 255
-        self.img_b = np.double(self.resizeimg(wvl_B, self.imagein[:, :, 2])) / 255
+        self.img_R = np.double(self.resizeimg(wvl_R, self.imagein[:, :, 0]))
+        self.img_G = np.double(self.resizeimg(wvl_G, self.imagein[:, :, 1]))
+        self.img_B = np.double(self.resizeimg(wvl_B, self.imagein[:, :, 2]))
+        self.img_r = np.double(self.resizeimg(wvl_B, self.imagein[:, :, 0]))
+        self.img_g = np.double(self.resizeimg(wvl_B, self.imagein[:, :, 1]))
+        self.img_b = np.double(self.resizeimg(wvl_B, self.imagein[:, :, 2]))
 
     def resizeimg(self, wvl, img):
         """RGB 파장에 맞게 원본 이미지를 리사이징 + zero padding"""
@@ -89,7 +88,7 @@ class FFT(Encoding):
         img_new[(h*2 - h_n) // 2:(h*2 + h_n) // 2, (w*2 - w_n) // 2:(w*2 + w_n) // 2] = im
         return img_new
 
-    def Frsn(self, color):
+    def Cal(self, color):
         if color == 'green':
             wvl = wvl_G
             image = self.img_G
@@ -101,13 +100,18 @@ class FFT(Encoding):
             image = self.img_R
         # resize image
         zzz = 1 * self.zz
-        ps = scaleX / w
+        ps = scaleX / (2*w)
         #psx = (wvl * self.zz) / (w * 10 * pp)
         #psy = (wvl * self.zz) / (10 * h * pp)
-        self.ch2 = image * h_frsn(ps, ps, w + w, h + h, zzz, wvl)
-        CH1 = np.fft.fft2(np.fft.fftshift(self.ch2))
-        result = CH1 * -h_frsn(pp, pp, w+w, h+h, zzz, wvl)
+        phase = np.random.random((2*h, 2*w)) * 2 * np.pi  # random phas
+        ph = np.exp(1j*phase)
+        ph *= image
+        self.ch2 = ph * h_frsn(ps, ps, w + w, h + h, zzz, wvl)
+        CH1 = np.fft.fftshift(np.fft.fft2(np.fft.fftshift(self.ch2)))
+        result = CH1 * h_frsn(pp, pp, w+w, h+h, zzz, wvl)
         result = result[h // 2: (3*h) // 2, w // 2: (3*w) // 2]
+        #phase = np.random.random((h, w)) * 2 * np.pi  # random phase
+        #result += np.exp(1j * phase)
         #result *= Refwave(wvl, zzz, self.thetaX, self.thetaY)
         return result
 
@@ -117,12 +121,79 @@ class FFT(Encoding):
             image = self.img_g
         elif color == 'blue':
             wvl = wvl_B
-            image = self.img_b
+            image = self.img_B
         else:
             wvl = wvl_R
             image = self.img_r
-        CH = self.fft(image)
+        phase = np.random.random((2 * h, 2 * w)) * 2 * np.pi  # random phas
+        ph = np.exp(1j * phase)
+        ph *= image
+        CH = self.fft(ph)
         CH = CH * asm_kernel(wvl, self.zz)
         result = self.ifft(CH)
         result = result[h // 2: (3 * h) // 2, w // 2: (3 * w) // 2]
+        phase = np.random.random((h,w)) * 2 * np.pi  # random phase
+        result += np.exp(1j * phase)
         return result
+
+
+    # functions
+    def fft(self, f):
+        return np.fft.fftshift(np.fft.fft2(np.fft.fftshift(f)))
+
+    def ifft(self, f):
+        return np.fft.fftshift(np.fft.ifft2(np.fft.fftshift(f)))
+
+    def SSB(self, ch):
+        """single side band encoding"""
+        height, width = ch.shape
+        a = np.zeros((height, width), dtype='complex128')
+        CH = self.fft(ch)  # fourier transformed image
+        CH = CH[height // 4: (height * 3) // 4, :]
+        a[0:height // 2, :] = CH
+        a[height // 2:, :] = np.conj(CH)
+        return self.ifft(a)
+
+    def normalize(self, arr, type='angle'):
+        """normalize"""
+        if type == 'phase':
+            arrin = np.copy(np.imag(arr))
+        elif type == 'real':
+            arrin = np.copy(np.real(arr))
+        elif type == 'angle':
+            arrin = np.copy(np.angle(arr))
+        elif type == 'amplitude':
+            arrin = np.copy(np.abs(arr))
+        else:
+            arrin = np.copy(arr)
+        # arrin = np.float(arrin)
+        arrin -= np.min(arrin)
+        # arrin = arrin + np.abs(arrin)
+        arrin = arrin / np.max(arrin)
+        return arrin
+
+    def getRGBImage(self, R, G, B, fname, type='phase'):
+        """Get RGB image"""
+        h, w = R.shape
+        img = np.zeros((h, w, 3))
+        img[:, :, 0] = self.normalize(R, type)
+        img[:, :, 1] = self.normalize(G, type)
+        img[:, :, 2] = self.normalize(B, type)
+        plt.imsave(fname, img)
+        return img
+
+    def getMonoImage(self, ch, fname):
+        """Get Single channel image"""
+        im = self.normalize(ch, 'phase')
+        phase = fname + '_IM.bmp'
+        plt.imsave(phase, im, cmap='gray')
+        re = self.normalize(ch, 'real')
+        real = fname + '_RE.bmp'
+        plt.imsave(real, re, cmap='gray')
+        return im, re
+
+
+if __name__ == '__main__':
+    fs = FFT('dice_rgb.bmp')
+    fs.getRGBImage(fs.Cal('red'), fs.Cal('green'), fs.Cal('blue'), '200915 2D FrsnFFT onaxis new1.bmp', type='angle')
+    fs.getRGBImage(fs.ASM('red'), fs.ASM('green'), fs.ASM('blue'), '200915 2D ASMFFT onaxis new1.bmp', type='angle')
